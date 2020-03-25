d5e9d9e7f3e1c2a40da8d98e12e1e6bd673278d0
Soaple <inje@soaple.io>
2020-03-25 20:12:33 +0900

    QueryManager implemented, and applied to SmartTable.

--- src/SmartTable.js
+++ src/SmartTable.js
@@ -3,6 +3,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import QueryManager from './QueryManager';
+import {
+    Table,
+    TableToolbar,
+    TableToolbarTitle,
+    TableHeader,
+    TableHead,
+    TableBody,
+    TableRowEmpty,
+    TableRow,
+    TableData,
+    TableFooter,
+} from './Table';
 
 const Wrapper = styled.div``;
 
@@ -10,11 +23,98 @@ class SmartTable extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {};
+        const endpoint = props.endpoint || `${window.location.origin}/graphql`;
+        QueryManager.initialize(endpoint);
+
+        // Generate header label dictionary
+        let headerLabelDict = {};
+        props.columns.forEach((column) => {
+            if (column.label) {
+                headerLabelDict[column.name] = column.label;
+            }
+        });
+
+        this.state = {
+            headerLabelDict: headerLabelDict,
+            // Data
+            rows: [],
+            // Table Pagination
+            rowsPerPage: 10,
+            page: 1,
+        };
     }
 
+    componentDidMount() {
+        this.readData();
+    }
+
+    readData = async () => {
+        const { rows, rowsPerPage } = this.state;
+        const { columns, queryName } = this.props;
+
+        const result = await QueryManager.query(`
+            {
+                ${queryName.readItems}(offset: ${rows.length}, limit: ${rowsPerPage}) {
+                    ${columns.map((column) => { return column.name }).join('\n')}
+                }
+            }`);
+
+        this.setState({
+            rows: result,
+        });
+    };
+
     render() {
-        return <Wrapper></Wrapper>;
+        const { headerLabelDict, rows, rowsPerPage, page } = this.state;
+        const { title } = this.props;
+
+        const totalPage = Math.ceil(rows.length / rowsPerPage);
+        const offset = Math.ceil((page - 1) * rowsPerPage);
+        const emptyRows = rowsPerPage - rows.length;
+
+        return (
+            <Table>
+                {/* Table Toolbar */}
+                <TableToolbar>
+                    <TableToolbarTitle>{title}</TableToolbarTitle>
+                </TableToolbar>
+
+                {/* Table Header */}
+                <TableHeader>
+                    {rows.length > 0 &&
+                        Object.keys(rows[0]).map((key, index) => {
+                            return (
+                                <TableHead key={index}>
+                                    {headerLabelDict[key] || key.toUpperCase()}
+                                </TableHead>
+                            );
+                        })}
+                </TableHeader>
+
+                {/* Table Body */}
+                <TableBody>
+                    {rows.map((item, index) => {
+                        return (
+                            <TableRow
+                                key={index}
+                                showBorderBottom={index < rows.length - 1}>
+                                {Object.keys(item).map((key, index) => {
+                                    return (
+                                        <TableData key={index}>
+                                            {item[key]}
+                                        </TableData>
+                                    );
+                                })}
+                            </TableRow>
+                        );
+                    })}
+                    {emptyRows > 0 && <TableRowEmpty numOfRows={emptyRows} />}
+                </TableBody>
+
+                {/* Table Footer */}
+                <TableFooter></TableFooter>
+            </Table>
+        );
     }
 }
 
